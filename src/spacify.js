0415cf5f74da4052db5efd1ec494521000aba740
Bruce <sanchez.uragan@gmail.com>
2018-04-26 14:15:04 +0300

    rewrite spacify function

--- src/spacify.js
+++ src/spacify.js
@@ -3,18 +3,6 @@
 const string = String;
 
 string.prototype.spacify = function () {
-    const val = this.valueOf();
-    const len = val.length;
-    let resString = '';
-    let i = 0;
-    if (val.length === 0) {
-        return '';
-    }
-    while (i < len - 1) {
-        resString += `${val[i]} `;
-        i += 1;
-    }
-    resString += val[len - 1];
-
-    return resString;
+    return this.split('').join(' ');
 };
+
